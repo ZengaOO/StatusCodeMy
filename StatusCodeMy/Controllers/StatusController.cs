0a745e5f27258465254a6b268531c11412adef06
ZengaOO <108860031+ZengaOO@users.noreply.github.com>
2023-08-24 23:54:51 +0300

    Add project files.

---
+++ StatusCodeMy/Controllers/StatusController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StatusCodeMy.St;
+
+namespace StatusCodeMy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatusController : Controller
+    {
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+       
+        public ActionResult<Status>GetStatus(int id)
+        {
+            if(id == 0)
+            { 
+                return BadRequest();
+
+            };
+           
+            return Ok();
+
+        }
+
+    }
+}
---
+++ StatusCodeMy/St/Status.cs
@@ -0,0 +1,8 @@
+﻿namespace StatusCodeMy.St
+{
+    public class Status
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
